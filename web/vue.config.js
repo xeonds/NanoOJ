564f2b225add102d4c6297ec58196acf7a367113
xeonds <1456506329@qq.com>
2022-04-10 23:24:14 +0800

    修复vue-cli3热更新

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    chainWebpack: config => {
+        config.resolve.symlinks(true) //修复热更新
+    }
 })
