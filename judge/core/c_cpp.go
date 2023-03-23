b3ec51e6d8a1aed26d1d4c2fe460c9b48d46406b
xeonds <1456506329@qq.com>
2023-03-23 18:21:27 +0800

    Use golang to rewrite C judger

---
+++ judge/core/c_cpp.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func (s *SourceCodeJudgement) cJudger() string {
+	compileCommand := "g++ -o " + s.Workdir + "/program " + s.SourceFile
+	compileResult, _ := exec.Command("bash", "-c", compileCommand).Output()
+	if len(compileResult) != 0 {
+		// TODO: return detailed info
+		return "Compilation failed"
+	}
+	var result string
+	for i := 0; i < len(s.InputFiles); i++ {
+		expectOutput, _ := os.ReadFile(s.ExpectFiles[i])
+		// Run the program with the given input
+		runCommand := s.Workdir + "/program < " + s.InputFiles[i]
+		programOutput, _ := exec.Command("bash", "-c", runCommand).Output()
+
+		// Compare the program output with the expected output
+		programOutputStr := strings.ReplaceAll(string(programOutput), "\r", "")
+		expectOutputStr := strings.ReplaceAll(string(expectOutput), "\r", "")
+		programOutputStr = strings.ReplaceAll(programOutputStr, "\n", "")
+		expectOutputStr = strings.ReplaceAll(expectOutputStr, "\n", "")
+		if programOutputStr == expectOutputStr {
+			result += fmt.Sprintf("Test case %d passed\n", i+1)
+		} else {
+			result += fmt.Sprintf("Test case %d failed\n", i+1)
+		}
+	}
+	return result
+}
---
+++ judge/core/main.go
@@ -0,0 +1,23 @@
+package core
+
+import "fmt"
+
+type SourceCodeJudgement struct {
+	Workdir     string
+	Lang        string
+	SourceFile  string
+	InputFiles  []string
+	ExpectFiles []string
+	TimeLimit   int
+}
+
+func (s *SourceCodeJudgement) Judge() (string, error) {
+	switch s.Lang {
+	case "c", "cpp":
+		return s.cJudger(), nil
+	// case "python":
+	// return judgement.pyJudger(), nil
+	default:
+		return "", fmt.Errorf("unsupported language: %s", s.Lang)
+	}
+}
