76bbd2310d74c024040a8105ff4b07832d43c58c
xeonds <1456506329@qq.com>
2023-03-28 23:09:37 +0800

    Set development proxy env

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   devServer: {
     proxy: {
-      "/api/v1/": {
+      "/api/v1": {
         target: "http://localhost:8080/api/v1/",
         changeOrigin: true,
-        // pathRewrite: {
-        //   "^/api": "",
-        // },
+        pathRewrite: {
+          "^/api/v1": "",
+        },
       },
     },
   },
