18054691c2e22b8b2bfc248e61a45c67ed1c9d07
xeonds <1456506329@qq.com>
2023-04-11 23:33:14 +0800

    Backend data model upgraded

--- judge/database/model/user.go
+++ judge/database/model/user.go
@@ -4,9 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID         uint16 `json:"id" gorm:"primaryKey;autoIncrement"`
-	Username   string `json:"username" gorm:"unique"`
-	Permission uint8  `json:"permission"`
-	Password   string `json:"password"`
-	Email      string `json:"email" gorm:"unique"`
+	ID          uint16       `json:"id" gorm:"primaryKey;autoIncrement"`
+	Username    string       `json:"username" gorm:"unique"`
+	Permission  uint8        `json:"permission"`
+	Password    string       `json:"password"`
+	Email       string       `json:"email" gorm:"unique"`
+	Submissions []Submission `json:"submissions" gorm:"foreignKey:UserID"`
+	Rank        uint64       `json:"rank"`
 }
