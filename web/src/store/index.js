80fdca5be95047753982609e4c4802139410151f
xeonds <1456506329@qq.com>
2023-08-23 21:55:28 +0800

    Frontend compile bug fix

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -5,48 +5,48 @@ import contest from "./modules/contest.js";
 import notification from "./modules/notification.js";
 import problem from "./modules/problem.js";
 import submission from "./modules/submission.js";
-import createPersistedState from "vuex-persistedstate";
 import jwtDecode from "jwt-decode";
 
 export default createStore({
-	actions: {
-		async init({ dispatch }) {
-			const token = localStorage.getItem("token");
-			if (token) {
-				const data = jwtDecode(token);
-				if (data.exp >= Date.now() / 1000) {
-					dispatch("logout");
-				}
-				dispatch("login", token);
-				dispatch("fetchUserInfo", data.user_id);
-			}
-			await dispatch("fetchUsers");
-			await dispatch("fetchContests");
-			await dispatch("fetchProblems");
-			await dispatch("fetchSubmissions");
-			await dispatch("fetchNotifications");
-		},
-		async login({ commit, dispatch }, token) {
-			commit("setToken", token);
-			commit("setLoggedInStatus", true);
-			localStorage.setItem("token", token);
-			const decode = jwtDecode(token);
-			await dispatch("fetchUserInfo", decode.user_id);
-		},
-		logout({ commit }) {
-			commit("setToken", "");
-			commit("setLoggedInStatus", false);
-			localStorage.removeItem("token");
-		},
-		async fetchRanks({ commit }) {
-			// TODO:use info from store.users, order users by rank
-		},
-	},
-	modules: {
-		problem, 
-		user,
-		info,
-		contest,
-		notification,
-		submission
-	});
+  actions: {
+    async init({ dispatch }) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const data = jwtDecode(token);
+        if (data.exp >= Date.now() / 1000) {
+          dispatch("logout");
+        }
+        dispatch("login", token);
+        dispatch("fetchUserInfo", data.user_id);
+      }
+      await dispatch("fetchUsers");
+      await dispatch("fetchContests");
+      await dispatch("fetchProblems");
+      await dispatch("fetchSubmissions");
+      await dispatch("fetchNotifications");
+    },
+    async login({ commit, dispatch }, token) {
+      commit("setToken", token);
+      commit("setLoggedInStatus", true);
+      localStorage.setItem("token", token);
+      const decode = jwtDecode(token);
+      await dispatch("fetchUserInfo", decode.user_id);
+    },
+    logout({ commit }) {
+      commit("setToken", "");
+      commit("setLoggedInStatus", false);
+      localStorage.removeItem("token");
+    },
+    async fetchRanks({ commit }) {
+      // TODO:use info from store.users, order users by rank
+    },
+  },
+  modules: {
+    problem,
+    user,
+    info,
+    contest,
+    notification,
+    submission,
+  },
+});
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -2,24 +2,57 @@
   <div>
     <el-row>
       <el-col :span="24">
-        <el-menu mode="horizontal" :ellipsis="false" :default-active="'1'" router>
+        <el-menu
+          mode="horizontal"
+          :ellipsis="false"
+          :default-active="'1'"
+          router
+        >
           <p id="logo">X-OJ</p>
-          <el-menu-item class="hidden-lg-and-up" index="1" route="/">主页</el-menu-item>
-          <el-menu-item class="hidden-lg-and-up" index="2" route="/problem">问题</el-menu-item>
-          <el-menu-item class="hidden-lg-and-up" index="3" route="/contest">竞赛/作业</el-menu-item>
-          <el-menu-item class="hidden-lg-and-up" index="4" route="/status">状态</el-menu-item>
-          <el-menu-item class="hidden-lg-and-up" index="5" route="/ranklist">排名</el-menu-item>
+          <el-menu-item class="hidden-lg-and-up" index="1" route="/"
+            >主页</el-menu-item
+          >
+          <el-menu-item class="hidden-lg-and-up" index="2" route="/problem"
+            >问题</el-menu-item
+          >
+          <el-menu-item class="hidden-lg-and-up" index="3" route="/contest"
+            >竞赛/作业</el-menu-item
+          >
+          <el-menu-item class="hidden-lg-and-up" index="4" route="/status"
+            >状态</el-menu-item
+          >
+          <el-menu-item class="hidden-lg-and-up" index="5" route="/ranklist"
+            >排名</el-menu-item
+          >
           <div class="flex-grow" />
-          <el-menu-item index="0" @click="this.$router.push('/editor')"><el-icon>
-              <EditPen />
-            </el-icon>在线代码编辑器</el-menu-item>
+          <el-menu-item index="0" @click="this.$router.push('/editor')"
+            ><el-icon>
+              <EditPen /> </el-icon
+            >在线代码编辑器</el-menu-item
+          >
           <el-sub-menu v-if="isLogin" index="1">
             <template #title>{{ username }}, 欢迎。</template>
-            <el-menu-item index="1-1" @click="this.$router.push('/profile')">个人中心</el-menu-item>
-            <el-menu-item index="1-2" @click="logout(); this.$router.push('/')">登出</el-menu-item>
-            <el-menu-item v-if="userPermission > 1" index="1-3" @click="this.$router.push('/admin')">管理</el-menu-item>
+            <el-menu-item index="1-1" @click="this.$router.push('/profile')"
+              >个人中心</el-menu-item
+            >
+            <el-menu-item
+              index="1-2"
+              @click="
+                logout();
+                this.$router.push('/');
+              "
+              >登出</el-menu-item
+            >
+            <el-menu-item
+              v-if="userPermission > 1"
+              index="1-3"
+              @click="this.$router.push('/admin')"
+              >管理</el-menu-item
+            >
           </el-sub-menu>
-          <el-menu-item v-else index="1" @click="this.$router.push('/login')">登录/注册</el-menu-item>
+          <el-menu-item v-else index="1" @click="this.$router.push('/login')"
+            >登录/注册</el-menu-item
+          >
         </el-menu>
       </el-col>
     </el-row>
@@ -27,21 +60,31 @@
       <el-col :lg="4" class="hidden-md-and-down">
         <el-affix>
           <el-menu id="vertical-menu" default-active="1" router>
-            <el-menu-item index="1" route="/"><el-icon>
-                <HomeFilled />
-              </el-icon>主页</el-menu-item>
-            <el-menu-item index="2" route="/problem"><el-icon>
-                <List />
-              </el-icon>问题</el-menu-item>
-            <el-menu-item index="3" route="/contest"><el-icon>
-                <Flag />
-              </el-icon>竞赛/作业</el-menu-item>
-            <el-menu-item index="4" route="/status"><el-icon>
-                <HelpFilled />
-              </el-icon>状态</el-menu-item>
-            <el-menu-item index="5" route="/ranklist"><el-icon>
-                <Histogram />
-              </el-icon>排名</el-menu-item>
+            <el-menu-item index="1" route="/"
+              ><el-icon>
+                <HomeFilled /> </el-icon
+              >主页</el-menu-item
+            >
+            <el-menu-item index="2" route="/problem"
+              ><el-icon>
+                <List /> </el-icon
+              >问题</el-menu-item
+            >
+            <el-menu-item index="3" route="/contest"
+              ><el-icon>
+                <Flag /> </el-icon
+              >竞赛/作业</el-menu-item
+            >
+            <el-menu-item index="4" route="/status"
+              ><el-icon>
+                <HelpFilled /> </el-icon
+              >状态</el-menu-item
+            >
+            <el-menu-item index="5" route="/ranklist"
+              ><el-icon>
+                <Histogram /> </el-icon
+              >排名</el-menu-item
+            >
           </el-menu>
         </el-affix>
       </el-col>
@@ -54,8 +97,6 @@
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import { useDark, useToggle } from "@vueuse/core";
-
 
 export default {
   created() {
@@ -64,7 +105,7 @@ export default {
   computed: {
     ...mapGetters({
       isLogin: "getLoggedInStatus",
-      userInfo: "getUserInfo"
+      userInfo: "getUserInfo",
     }),
     username: function () {
       return this.userInfo.username;
@@ -75,16 +116,16 @@ export default {
   },
   methods: {
     ...mapActions({
-      _logout: "logout"
-    })
-    , logout: function () {
+      _logout: "logout",
+    }),
+    logout: function () {
       this._logout();
       this.$router.push("/");
-    }
-  }
+    },
+  },
 };
 </script>
- 
+
 <style>
 #vertical-menu {
   min-width: 12rem;
@@ -120,4 +161,3 @@ body {
   padding: 0px !important;
 }
 </style>
-
